09290ccde3fa12c3d4037b34e7b1e563869bba96
Katiasun <katjatolok3@gmail.com>
2024-04-20 18:56:14 +0200

    create button and function for it rollBtn

--- src/App.js
+++ src/App.js
@@ -12,9 +12,16 @@ export default function App() {
 
   const [dice, setDice] = useState(allNewDice());
   const diceElements = dice.map((die) => <Die value={die} />);
+
+  function rollBtn() {
+    setDice(allNewDice());
+  }
   return (
     <main>
       <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollBtn}>
+        Roll
+      </button>
     </main>
   );
 }
