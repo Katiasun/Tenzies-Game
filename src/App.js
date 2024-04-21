c8864d1fff0d384da09a2e179c10ed46639a7187
Katiasun <katjatolok3@gmail.com>
2024-04-21 15:29:25 +0200

    Allow the user to play a new game when the button is clicked and they've already won

--- src/App.js
+++ src/App.js
@@ -28,11 +28,16 @@ export default function App() {
   ));
 
   function rollBtn() {
-    setDice((prevRoll) =>
-      prevRoll.map((die) => {
-        return die.isHeld ? die : generateNewDie();
-      })
-    );
+    if (!tenzies) {
+      setDice((prevRoll) =>
+        prevRoll.map((die) => {
+          return die.isHeld ? die : generateNewDie();
+        })
+      );
+    } else {
+      setTenzies(false);
+      setDice(allNewDice());
+    }
   }
 
   function holdDice(id) {
@@ -47,9 +52,15 @@ export default function App() {
     const allSameValue = dice.every((die) => die.value === firstValue);
     if (allHeld && allSameValue) {
       setTenzies(true);
-      console.log("You won!");
     }
   }, [dice]);
+
+  // function checkWin() {
+  //   const allHeld = dice.every((die) => die.isHeld);
+  //   if (allHeld) {
+  //     setTenzies(true);
+  //   }
+  // }
 
   return (
     <main>
